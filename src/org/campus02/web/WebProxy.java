3a30e675b4c6dc9cc62ebe8c79469fb413ac99bb
Mario Skedelj <skedelj.mario@gmx.at>
2021-05-03 02:46:32 +0200

    WebProxy class adapted

--- src/org/campus02/web/WebProxy.java
+++ src/org/campus02/web/WebProxy.java
@@ -1,6 +1,11 @@
 package org.campus02.web;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
 
 public class WebProxy {
     private PageCache cache;
@@ -16,13 +21,47 @@ public class WebProxy {
     }
 
     public WebPage fetch(String url) throws UrlLoaderException{
-        try {
-            HashMap<String, WebPage> cacheMap = cache.getCache();
-            if (cacheMap.get(url) == null)
-                WebPage pageFromCache = cache.readFromCache(url);
-        } catch (CacheMissException e) {
-            e.printStackTrace();
+        WebPage webPage = null;
+        try{
+            webPage = cache.readFromCache(url);
+            numCacheHits++;
         }
-        return null;
+        catch(CacheMissException ex){
+            webPage = UrlLoader.loadWebPage(url);
+            PageCache pageCache = new PageCache();
+            pageCache.writeToCache(webPage);
+            numCacheMisses++;
+        }
+        return webPage;
+    }
+
+    public String statsHits(){
+        return "stats hits: " + numCacheHits;
+    }
+
+    public String statsMisses(){
+        return "stats misses: " + numCacheMisses;
+    }
+
+    public boolean writePageCacheToFile(String pathToFile){
+        HashMap<String,WebPage> cacheForFile = cache.getCache();
+        File f = null;
+        FileWriter fw = null;
+        PrintWriter pw = null;
+        try
+        {
+            f = new File(pathToFile);
+            fw = new FileWriter(f);
+
+            for (Map.Entry<String, WebPage> entry : cacheForFile.entrySet()) {
+                pw.println(entry.getKey() +";"+entry.getValue());
+            }
+            return true;
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
